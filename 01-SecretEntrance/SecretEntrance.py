0f01337661853e28689307f4e9a94e76703a4a19
edh <edouard.hinard@orange.com>
2025-12-01 15:24:04 +0100

    ruff

--- 01-SecretEntrance/SecretEntrance.py
+++ 01-SecretEntrance/SecretEntrance.py
@@ -5,6 +5,7 @@ import pathlib
 DIAL_COUNT = 100
 START_POSITION = 50
 
+
 def part1():
     dial = START_POSITION
     with pathlib.Path("input.txt").open() as f:
@@ -21,7 +22,7 @@ def part1():
             if dial == 0:
                 yield 1
 
-print(f"Part #1: {sum(part1())}")
+
 def part2():
     dial = START_POSITION
     with pathlib.Path("input.txt").open() as f:
@@ -29,7 +30,7 @@ def part2():
             rotation = int(line[1:])
 
             # +1 each time the arrow makes a whole turn
-            yield  rotation // DIAL_COUNT
+            yield rotation // DIAL_COUNT
             rotation %= DIAL_COUNT
 
             if rotation == 0:
@@ -46,4 +47,6 @@ def part2():
                         yield 1
                     dial = (dial + rotation) % DIAL_COUNT
 
+
+print(f"Part #1: {sum(part1())}")
 print(f"Part #2: {sum(part2())}")
